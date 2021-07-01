eda565c1d440ec89d83748cc216bd599b70839cb
Sumit Chutani <sumitengineer23@gmail.com>
2021-07-02 00:10:33 +0530

    Edited contact.php file

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'sumitchutani50@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -26,8 +26,8 @@
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
+    'host' => 'sumitchutani50@gmail.com',
+    'username' => 'sumit-codebrewer',
     'password' => 'pass',
     'port' => '587'
   );
