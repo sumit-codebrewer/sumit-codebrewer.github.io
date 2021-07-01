da4157ddffeb1781daee12931eac6862553914b9
Sumit Chutani <sumitengineer23@gmail.com>
2021-07-02 00:16:37 +0530

    Modified contact section

--- forms/contact.php
+++ forms/contact.php
@@ -6,7 +6,7 @@
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
-  // Replace contact@example.com with your real receiving email address
+ 
   $receiving_email_address = 'sumitchutani50@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
